623519b89ee46ac4a1000031bd3bddb2e9b81540
Ricardo Fahham <rfahham@hotmail.com>
2025-02-21 12:00:20 -0300

    Scripts de Testes com K6

--- testes/robustez.js
+++ testes/robustez.js
@@ -10,7 +10,7 @@ export const options = {
 };
 
 export default function () {
-  const res = http.get('https://quickpizza.grafana.com/');
+  const res = http.get('http://localhost/');
   check(res, { 'status was 200': (r) => r.status == 200 });
   sleep(1);
 }

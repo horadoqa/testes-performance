06852e7051c6723243acb964af44da45e52c23bd
Ricardo Fahham <rfahham@hotmail.com>
2025-02-26 16:03:39 -0300

    Atualizando

--- testes/serverest/usuarios/listar-todos.js
+++ testes/serverest/usuarios/listar-todos.js
@@ -2,7 +2,16 @@ import http from 'k6/http';
 import { check } from 'k6';
 
 // URL da API para obter a lista de usuários
-const url = 'https://34.229.246.171/usuarios';
+const url = 'http://<IP>:3000/usuarios';
+
+
+export const options = {
+  stages: [
+    { duration: '60s', target: 10 },
+    { duration: '4m', target: 10 },
+    { duration: '30s', target: 0 },
+  ],
+};
 
 // Cabeçalhos da requisição
 const headers = {
@@ -16,9 +25,9 @@ export default function () {
   // Verifica se o status da resposta é 200 (OK)
   check(res, {
     'status é 200': (r) => r.status === 200,
-    'resposta contém um JSON': (r) => r.headers['Content-Type'] === 'application/json',
+    // 'resposta contém um JSON': (r) => r.headers['Content-Type'] === 'application/json',
   });
 
   // Opcional: imprime a resposta para depuração
-  console.log('Resposta:', res.body);
+  // console.log('Resposta:', res.body);
 }
